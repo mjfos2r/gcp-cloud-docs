f5c4ac00ce70dbd97f79c36561df24fccf805edd
Michael Foster <50351736+mjfos2r@users.noreply.github.com>
2025-06-12 23:40:06 -0400

    formatted and linted.
    Signed-off-by: Michael Foster <50351736+mjfos2r@users.noreply.github.com>

--- storage/useful_functions.py
+++ storage/useful_functions.py
@@ -1,3 +1,9 @@
+import io
+
+import pandas
+from google.storage import storage
+
+
 def gs_download_to_stream(bucket_name, source_blob_name, file_obj):
     # to download object: gs://my_bucket/path/to/file/in/bucket/file.txt
     #     bucket_name == "my_bucket"
@@ -10,15 +16,20 @@ def gs_download_to_stream(bucket_name, source_blob_name, file_obj):
     print(f"Downloaded blob: {source_blob_name} \n to file-like object/stream.\n")
     return file_obj
 
+
 def gs_dataframe_from_bucket(bucket_name, source_blob_name, delimiter=","):
     """Downloads a file from a bucket and converts to pandas.DataFrame, defaults to csv"""
     io_obj = io.BytesIO()
     raw_stream = gs_download_to_stream(bucket_name, source_blob_name, io_obj)
     raw_stream.seek(0)
-    file_content = raw_stream.getvalue().decode('utf-8')
-    df = pandas.read_csv(io.StringIO(file_content), delimiter=delimiter, encoding='utf-8')
+    df = pandas.read_csv(
+        io.StringIO(raw_stream.getvalue().decode("utf-8")),
+        delimiter=delimiter,
+        encoding="utf-8",
+    )
     print("Created dataframe from filestream")
     return df
+
 
 def gs_rename_blob(bucket_name, blob_name, new_name):
     """
@@ -35,6 +46,7 @@ def gs_rename_blob(bucket_name, blob_name, new_name):
 
     new_blob = bucket.rename_blob(blob, new_name)
     print(f"Blob {blob.name} was renamed to {new_blob.name}")
+
 
 def gs_list_blobs(bucket, prefix, delimiter=None, loud=False):
     storage_client = storage.Client()
@@ -53,6 +65,7 @@ def gs_list_blobs(bucket, prefix, delimiter=None, loud=False):
                 print(prefix)
     return blobs
 
+
 def gs_upload_blob_from_stream(bucket_name, file_obj, destination_blob_name):
     """upload from a stream to a file in the bucket."""
     storage_client = storage.Client()
@@ -61,4 +74,3 @@ def gs_upload_blob_from_stream(bucket_name, file_obj, destination_blob_name):
     file_obj.seek(0)
     blob.upload_from_file(file_obj)
     print(f"Stream data uploaded to {destination_blob_name} in bucket {bucket_name}")
-
